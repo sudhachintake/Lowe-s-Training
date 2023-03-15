062498a3ba45446d986d5e485bc9b95ce9738c36
sudhachintake <79921239+sudhachintake@users.noreply.github.com>
2023-03-15 10:07:03 +0530

    Add files via upload

---
+++ List.java
@@ -0,0 +1,17 @@
+package com.sudha;
+
+import java.util.*;
+
+public class List {
+	public static void main(String[] args) {
+		
+		List<Integer> l=new ArrayList<>();
+		1.add (19);
+		1.add (94);
+		1.add (77);
+		1.add (25);
+		1.add (86);
+		System.out.println("Max element"+Collections.max(1));
+		System.out.println("Min element"+Collections.min(1));
+	}
+}
